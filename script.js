0c117a8e69c8af9157e78f4bed02ce982027fe6e
Oyedele oreoefeoluwa <oyedeleoreofe@gmail.com>
2022-09-22 13:17:30 +0100

    created who wants to be a millionaire

---
+++ script.js
@@ -0,0 +1,42 @@
+var questionBank = [
+    {
+        question: 'pick  the odd one out',
+        option : ['me','you','she','he'],
+        answer :  'you'
+    },
+      {
+        question: 'whats the capital of oyo',
+        option : ['ibadan','jos','kano','osogbo'],
+        answer :  'ibadan'
+    },  
+
+    {
+        question: 'whats 2 + 2',
+        option : ['4','6','8','10'],
+        answer :  '4'
+    },
+    {
+        question: 'how old are you?',
+        option : ['3','5','6','i dont know'],
+        answer :  ' i dont know'
+    },
+    {
+        question: 'whats the name of americas current president',
+        option : ['joe biden','donald trump','buhari','Abraham lincon'],
+        answer :  'joe biden'
+    },
+    {
+        question: '"who is your daddy", which nollywood movie has that phrase',
+        option : ['chief daddy','cook daddy','king daddy','police daddy'],
+        answer :  ''
+    },
+    
+]
+var question = document.getElementById('question');
+var question = document.getElementById('question');
+var question = document.getElementById('question');
+var question = document.getElementById('question');
+var question = document.getElementById('question');
+var question = document.getElementById('question');
+var question = document.getElementById('question');
+var question = document.getElementById('question');
