79c4c834940583c194bbaa9adcc8242d56f5262f
Oyedele oreoefeoluwa <oyedeleoreofe@gmail.com>
2022-10-08 12:01:28 +0100

    added a function

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ var questionBank = [
     {
         question: 'how old are you?',
         option: ['3', '5', '6', 'i dont know'],
-        answer: ' i dont know'
+        answer: 'i dont know'
     },
     {
         question: 'whats the name of americas current president',
@@ -38,14 +38,16 @@ var questionBank = [
 
 ]
 
-   
-   let remove = new Audio("./extras/50-50_sound.mp3")
-   let PhoneS = new Audio("./extras/soundb.mp3")
-   let audiE  = new Audio("./extras/Ask The Audience_sound.mp3")
-   
-   
-   var question = document.getElementById('question');
-   var quizContainer = document.getElementById('quiz-container');
+
+let remove = new Audio("./extras/50-50_sound.mp3")
+let PhoneS = new Audio("./extras/soundb.mp3")
+let audiE = new Audio("./extras/Ask The Audience_sound.mp3")
+let correct = new Audio('./extras/sound_correct.mp3')
+let inCorrect = new Audio('./extras/sound_incorrect.mp3')
+
+
+var question = document.getElementById('question');
+var quizContainer = document.getElementById('quiz-container');
 var scorecard = document.getElementById('scoreboard');
 var option0 = document.getElementById('option0');
 var option1 = document.getElementById('option1');
@@ -60,7 +62,7 @@ var score = 0;
 
 // function displayQuestion() {
 //        for(var a=0; a<span.length;a++){
-    // span[a].style.background='none';
+// span[a].style.background='none';
 // }
 //     question.innerHTML = ` <p class="text-center">Q.${i + 1}</p> <div class="quest">${questionBank[i].question}</div> `
 //     option0.innerHTML =  ` <div class="choose"> A. ${questionBank[i].option[0]}</div>`;
@@ -69,75 +71,87 @@ var score = 0;
 //     option3.innerHTML = `<div class="choose">D. ${questionBank[i].option[3]}</div>`;
 //     statusbar.innerHTML = "Question" + '' + (i + 1) + '' + 'of' + '' + questionBank.length
 // }
-function displayQuestion(){
-    for(var a=0; a<span.length;a++){
-        span[a].style.background='none';
-}
- question.innerHTML = ` <p class="text-center">Q.${i + 1}</p> <div class="quest">${questionBank[i].question}</div> `
+function displayQuestion() {
+    for (var a = 0; a < span.length; a++) {
+        span[a].style.background = 'none';
+    }
+    question.innerHTML = ` <p class="text-center">Q.${i + 1}</p> <div class="quest">${questionBank[i].question}</div> `
 
-option0.innerHTML=questionBank[i].option[0];
-option1.innerHTML=questionBank[i].option[1];
-option2.innerHTML=questionBank[i].option[2];
-option3.innerHTML=questionBank[i].option[3];
-statusbar.innerHTML="question"+''+(i+1)+''+'of'+''+questionBank.length;
+    option0.innerHTML = questionBank[i].option[0];
+    option1.innerHTML = questionBank[i].option[1];
+    option2.innerHTML = questionBank[i].option[2];
+    option3.innerHTML = questionBank[i].option[3];
+    statusbar.innerHTML = "question" + '' + (i + 1) + '' + 'of' + '' + questionBank.length;
 }
 //function to calculaate scores
 function calScore(e) {
     if (e.innerHTML === questionBank[i].answer && score < questionBank.length) {
-        score= score+1;
+        score = score + 1;
+        correct.play();
         document.getElementById(e.Id);
-        e.innerHTML=  `<div width="50px height="50px" style="background-color:green">correct</div>`
-        
-    }
-    else{
-        document.getElementById(e.Id)
-        e.innerHTML=  `<div width="50px height="50px" style="background-color:red;">wrong</div>`
-        
-    }
-    setTimeout(nextQuestion, 300 );
-}
-var enabled= true
-function removeOptions(){
-  if(enabled==true){  remove.play();
-    option3.innerHTML = `<div class="choose"></div>`;
-    option2.innerHTML = `<div class="choose"></div>`;
-    fifty.innerHTML = ` <img src="./extras/50-50-used.png" alt="">`;
-    enabled=false
-}else{
-setTimeout(() => {    
-    stat.innerHTML=`function already used`
-    setTimeout(() => {
-       stat.innerHTML=`` 
-    }, 2000);
-}, 300);   
- 
-}}
+        e.innerHTML = `<div width="50px height="50px" style="background-color:green">correct</div>`
 
-var enable= true
-function callFriend(){
-    if(enable==true){
-    audiE.play();
-    window.location.href= "tel:"
-    phone.innerHTML = ` <img src="./extras/phone-a-friend-used.png" alt="">`
-    enable=false
+    }
+    else {
+        document.getElementById(e.Id)
+        e.innerHTML = `<div width="50px height="50px" style="background-color:red;">wrong</div>`
+        inCorrect.play()
+
+        if (inCorrect.play()) { 
+            setTimeout(() => {
+                restartGame() 
+            }, 1000); 
+        }
+
+    }
+    setTimeout(nextQuestion, 300);
 }
+var enabled = true
+function removeOptions() {
+    if (enabled == true) {
+        remove.play();
+        option3.innerHTML = `<div class="choose"></div>`;
+        option2.innerHTML = `<div class="choose"></div>`;
+        fifty.innerHTML = ` <img src="./extras/50-50-used.png" alt="">`;
+        enabled = false
+    } else {
+        setTimeout(() => {
+            stat.innerHTML = `function already used`
+            setTimeout(() => {
+                stat.innerHTML = ``
+            }, 2000);
+        }, 300);
+
+    }
 }
-function restartGame(){cdxc
+const playSong = () => {
+    PhoneS.play()
+}
+var enable = true
+function callFriend() {
+    if (enable == true) {
+        audiE.play();
+        window.location.href = "tel:"
+        phone.innerHTML = ` <img src="./extras/phone-a-friend-used.png" alt="">`
+        enable = false
+    }
+}
+function restartGame() {
     location.reload()
 }
 
 // function to display next question
-function nextQuestion(){
-    if (i<questionBank.length-1){
-        i=i+1;
+function nextQuestion() {
+    if (i < questionBank.length - 1) {
+        i = i + 1;
         displayQuestion();
-        checkAnswer.innerHTML= "er"
+        checkAnswer.innerHTML = "er"
     }
-    else{
-        points.innerHTML=score+'/'+
-        questionBank.length;
-        quizContainer.style .display= 'none';
-        scoreboard.style.display= 'block'
+    else {
+        points.innerHTML = score + '/' +
+            questionBank.length;
+        quizContainer.style.display = 'none';
+        scoreboard.style.display = 'block'
 
     }
 }
@@ -148,23 +162,23 @@ next.addEventListener('click', nextQuestion);
 
 // back to quiz button event
 
-function backToQuiz(){
+function backToQuiz() {
     location.href = "index.html";
 }
 
 // function to check answers
 
-function checkAnswer(){
+function checkAnswer() {
     var answeBank = document.getElementById('answerBank');
-    var answers= document.getElementById('answers');
-    answeBank.style.display ='block';
-    scoreboard.style.display ='none';
-    
-        for(var a =0; a<questionBank.length; a++){
-            var list= document.createElement('li');
-            list.innerHTML=questionBank[a].answer;
-            answers.appendChild(list);
-        }
+    var answers = document.getElementById('answers');
+    answeBank.style.display = 'block';
+    scoreboard.style.display = 'none';
+
+    for (var a = 0; a < questionBank.length; a++) {
+        var list = document.createElement('li');
+        list.innerHTML = questionBank[a].answer;
+        answers.appendChild(list);
+    }
 
 }
 
